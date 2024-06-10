af9f7ba737219173cd9746692f5869431ccaecc2
hhhallan <allanstm76@gmail.com>
2024-06-11 00:21:07 +0200

    contact: refacto

---
+++ src/app/(marketing)/(home)/_components/contact.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { LInput } from "@/components/form/l-input";
+import { Button } from "@/components/ui/button";
+
+type Props = {};
+
+export const Contact = ({}: Props) => {
+  return (
+    <section id="contact" className="section bg-gray-100">
+      <div className="mx-auto grid max-w-[1400px] grid-cols-1 gap-x-16 gap-y-8 px-4 md:px-6 lg:grid-cols-5">
+        <div className="lg:col-span-2 lg:py-12">
+          <h3 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl">
+            Nous contacter
+          </h3>
+          <p className="description mt-3 max-w-xl">
+            Vous avez des questions ou besoin d&apos;une consultation
+            personnalisée? Contactez-nous dès aujourd&apos;hui pour discuter de
+            vos besoins et découvrir comment Qualissee peut vous aider à
+            atteindre vos objectifs.
+          </p>
+
+          <div className="mt-8">
+            <a href="#" className="text-2xl font-bold text-pink-600">
+              +33 1 23 45 67 89
+            </a>
+
+            <address className="mt-2 not-italic">
+              123 Rue Fictive, 75001 Paris, France
+            </address>
+          </div>
+        </div>
+
+        <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+          <form action="#" className="space-y-4">
+            <LInput name="email" type="email" label="Email" />
+
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+              <LInput name="Nom" type="text" label="Nom" />
+              <LInput name="Prénom" type="text" label="Prénom" />
+            </div>
+
+            <LInput
+              label="Message"
+              type="textarea"
+              name="message"
+              placeholder="Ecrivez votre message ici..."
+              message
+            />
+
+            <div className="mt-4">
+              <Button type="submit">Envoyer</Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/app/(marketing)/(home)/page.tsx
+++ src/app/(marketing)/(home)/page.tsx
@@ -4,18 +4,17 @@ import {
   SectionTitle,
 } from "@/components/section";
 import { Hero } from "./_components/hero";
-import { PrestaCard } from "./_components/prestaCard";
-import { PrestaList } from "./_components/prestaList";
+import { PrestaCard } from "./_components/presta-card";
+import { PrestaList } from "./_components/presta-list";
 import { ExcellenceCTA } from "./_components/excellence";
 import { List } from "@/components/list";
 import { ListItem } from "@/components/list-item";
 import { SeeMore } from "./_components/see-more";
 import { SeeMoreItem } from "./_components/see-more-item";
 import { Certification } from "@/components/certification";
-import { LInput } from "@/components/form/l-input";
-import { Button } from "@/components/ui/button";
 import { services } from "@/constants/services";
 import { qhse } from "@/constants/qhse";
+import { Contact } from "./_components/contact";
 
 const HomePage = () => {
   return (
@@ -46,6 +45,7 @@ const HomePage = () => {
             title={service.title}
             description={service.description}
             mediaSrc={service.mediaSrc}
+            href={`/services/${service.id}`}
           />
         ))}
       </PrestaList>
@@ -74,55 +74,7 @@ const HomePage = () => {
       <Certification />
 
       {/* Contact */}
-      {/* TODO: refacto le comp (reusable) */}
-      <section id="contact" className="section bg-gray-100">
-        <div className="mx-auto grid max-w-[1400px] grid-cols-1 gap-x-16 gap-y-8 px-4 md:px-6 lg:grid-cols-5">
-          <div className="lg:col-span-2 lg:py-12">
-            <h3 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl">
-              Nous contacter
-            </h3>
-            <p className="description mt-3 max-w-xl">
-              Vous avez des questions ou besoin d&apos;une consultation
-              personnalisée? Contactez-nous dès aujourd&apos;hui pour discuter
-              de vos besoins et découvrir comment Qualissee peut vous aider à
-              atteindre vos objectifs.
-            </p>
-
-            <div className="mt-8">
-              <a href="#" className="text-2xl font-bold text-pink-600">
-                +33 1 23 45 67 89
-              </a>
-
-              <address className="mt-2 not-italic">
-                123 Rue Fictive, 75001 Paris, France
-              </address>
-            </div>
-          </div>
-
-          <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-            <form action="#" className="space-y-4">
-              <LInput name="email" type="email" label="Email" />
-
-              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                <LInput name="Nom" type="text" label="Nom" />
-                <LInput name="Prénom" type="text" label="Prénom" />
-              </div>
-
-              <LInput
-                label="Message"
-                type="textarea"
-                name="message"
-                placeholder="Ecrivez votre message ici..."
-                message
-              />
-
-              <div className="mt-4">
-                <Button type="submit">Submit</Button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </section>
+      <Contact />
     </>
   );
 };
