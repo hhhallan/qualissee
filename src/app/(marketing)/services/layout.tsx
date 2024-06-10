9ff0b1eac56bbd159a909340bba3f4d18baf2d60
hhhallan <allanstm76@gmail.com>
2024-06-10 20:49:43 +0200

    service: page detail updated

--- src/app/(marketing)/services/layout.tsx
+++ src/app/(marketing)/services/layout.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const ServiceLayout = ({ children }: Props) => {
   return (
-    <div className="flex min-h-[80vh] flex-col items-center justify-center md:mt-0 lg:mt-11">
+    <div className="mt-11 flex min-h-[80vh] flex-col items-center justify-center">
       {children}
     </div>
   );
