fff458ea9b841a1d2866b030916909f433f601fb
hhhallan <allanstm76@gmail.com>
2024-06-14 14:07:06 +0200

    presta: ajout externalisation + responsive

--- src/app/(marketing)/(home)/_components/presta-card.tsx
+++ src/app/(marketing)/(home)/_components/presta-card.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export const PrestaCard = ({ title, description, mediaSrc, href }: Props) => {
   return (
-    <div className="group relative overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:shadow-xl">
+    <div className="group relative flex flex-col justify-between overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:shadow-xl">
       <Link href={href} className="absolute inset-0 z-10" prefetch={false}>
         <span className="sr-only">View</span>
       </Link>
@@ -22,13 +22,18 @@ export const PrestaCard = ({ title, description, mediaSrc, href }: Props) => {
         height={400}
         className="h-64 w-full object-cover"
       />
-      <div className="bg-white p-4 dark:bg-gray-950">
+      <div className="flex-grow bg-white p-4 dark:bg-gray-950">
         <h3 className="text-xl font-bold">{title}</h3>
         <p className="text-sm text-gray-500 dark:text-gray-400">
           {description}
         </p>
-        <Button size="sm" className="mt-4">
-          J&apos;opte pour &quot;{title}&quot;
+      </div>
+      <div className="p-4">
+        <Button size="sm" className="w-full" asChild>
+          <span className="py-4 xl:py-6">
+            J&apos;opte pour <br className="hidden xl:block" />
+            &quot;{title}&quot;
+          </span>
         </Button>
       </div>
     </div>
--- src/app/(marketing)/(home)/_components/presta-list.tsx
+++ src/app/(marketing)/(home)/_components/presta-list.tsx
@@ -15,7 +15,7 @@ export const PrestaList = ({ children }: Props) => {
             besoins.
           </p>
         </div>
-        <div className="mt-12 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <div className="mt-12 grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
           {children}
         </div>
       </div>
