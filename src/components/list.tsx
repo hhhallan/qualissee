922e4bfd7fd000454128917dc6e1c0b12dd7f6de
hhhallan <allanstm76@gmail.com>
2024-06-14 20:08:52 +0200

    list: refacto du code

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,19 +1,33 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+
 type Props = {
   children: React.ReactNode;
   title: string;
   description: string;
+  id?: string;
+  secondary?: boolean;
 };
-export const List = ({ children, title, description }: Props) => {
+export const List = ({
+  children,
+  title,
+  description,
+  id,
+  secondary,
+}: Props) => {
+  const activeClass = secondary
+    ? "sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5"
+    : "md:grid-cols-2 lg:grid-cols-4";
+
   return (
-    <section className="section">
-      <div className="container px-4 md:px-6">
+    <section id={id || ""} className="section">
+      <div className="container px-4 text-center md:px-6">
         <div className="space-y-3 text-center">
           <h2 className="title">{title}</h2>
           <p className="description mx-auto max-w-[700px]">{description}</p>
         </div>
-        <div className="mt-12 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <div className={cn("mt-12 grid grid-cols-1 gap-6", activeClass)}>
           {children}
         </div>
       </div>
